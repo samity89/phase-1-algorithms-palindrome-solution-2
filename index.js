c0cbf0ead5cd2cb490ea5341f96e8d3a56694079
Samuel Amity <sam.amity@gmail.com>
2022-09-08 13:58:44 -0400

    done

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function isPalindrome(word) {
   for (let i = 0; i < word.length / 2; i++) {
     const j = word.length - 1 - i;
-    const firstChar = word[i]
-    const lastChar = word[j]
-    if (firstChar !== lastChar) return false
+    if (word[i] !== word[j]) return false
   }
   return true
 }
